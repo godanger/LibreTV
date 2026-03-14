ef61cb72bbc74e71b664092b18d5638c87374db0
godanger <godanger@gmail.com>
2026-03-14 14:07:19 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,44 +1,5 @@
 const CUSTOMER_SITES = {
-    ysgc: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod/',
-        name: '影视工厂'
-    },
-    subo: {
-        api: 'https://subocaiji.com/api.php/provide/vod',
-        name: '速博资源'
-    },
-    1080zy: {
-        api: 'https://api.1080zyku.com/inc/api_mac10.php',
-        name: '1080资源'
-    },
-    bdzy: {
-        api: 'https://api.apibdzy.com/api.php/provide/vod',
-        name: '百度资源'
-    },
-    uku: {
-        api: 'https://api.ukuapi.com/api.php/provide/vod',
-        name: 'U酷资源'
-    },
-    yayazy: {
-        api: 'https://cj.yayazy.net/api.php/provide/vod',
-        name: '丫丫点播'
-    },
-    suoni: {
-        api: 'https://suoniapi.com/api.php/provide/vod',
-        name: '索尼资源'
-    },
-    hongniu: {
-        api: 'https://www.hongniuzy2.com/api.php/provide/vod',
-        name: '红牛资源'
-    },
-    huya: {
-        api: 'https://www.huyaapi.com/api.php/provide/vod',
-        name: '虎牙资源'
-    },
-    maoyan: {
-        api: 'https://api.maoyanapi.top/api.php/provide/vod/at/json',
-        name: '猫眼资源'
-    },
+
     jyzy: {
         api: 'https://jyzyapi.com/provide/vod',
         name: '金鹰资源'
